ec4600e7a57111a1be1d37b6bbe904975bdf41f6
mahmoudmaged226 <mahmoudmaged226@gmail.com>
2025-01-18 20:39:00 +0200

    Refactor entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
-import { Game } from "../entities/Game";
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import  Game  from "../entities/Game";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
